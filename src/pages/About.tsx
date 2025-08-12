edffa3275873f54556bb2b0df97f6eaec71a5f19
Donna Chow <dchow@indeed.com>
2025-08-12 13:10:56 -0400

    Add all names into one column

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,8 @@
 import type { JSX } from "react";
 import styles from "./About.module.css";
 
+// TODO: Make this collapsable and expandable
+
 const About = (): JSX.Element => {
   return (
     <div>
@@ -20,6 +22,7 @@ const About = (): JSX.Element => {
 
       <div>
         <h2>2023 - 2025 Board Members</h2>
+        {/*Priest first picture, and then 2 rows of 5*/}
         <div className={styles.boardMemberContainer}>
           <div className={styles.boardMember}>
             <img src="img1.png" alt="Board Member 1" />
@@ -41,32 +44,60 @@ const About = (): JSX.Element => {
 
       <div className={styles.pastBoardMembersContainer}>
         <h2>Past Board Members</h2>
-        <div className={styles.pastBoardMembers}>
-          <p>2020 - 2022 Board Member</p>
-          <ul>
-            <li>Board Member 1</li>
-            <li>Board Member 2</li>
-            <li>Board Member 3</li>
-          </ul>
-          <ul>
-            <li>Board Member 4</li>
-            <li>Board Member 5</li>
-            <li>Board Member 6</li>
-          </ul>
-        </div>
-        <div className={styles.pastBoardMembers}>
-          <p>2017 - 2019 Board Member</p>
-          <ul>
-            <li>Board Member 1</li>
-            <li>Board Member 2</li>
-            <li>Board Member 3</li>
-          </ul>
-          <ul>
-            <li>Board Member 4</li>
-            <li>Board Member 5</li>
-            <li>Board Member 6</li>
-          </ul>
-        </div>
+        <p><i>Legend: Rev - Reverend, Tr. - Trưởng, TT. - Trợ Tá</i></p>
+          <div className={styles.pastBoardMembers}>
+            <p>2021-2023 Chapter Executive Committee Members</p>
+            <ul>
+              <li>Cha Tuyên Uý - Chapter Chaplain: Rev. Philip Phan Xuân Lành</li>
+              <li>Đoàn Trưởng - Chapter President: Tr. Joan of Arc Trần Valentina</li>
+              <li>Đoàn Phó Quản Trị - Chapter VP of Administrative Affairs: Tr. Đôminicô Trạch Trần HuyThành</li>
+              <li>Đoàn Phó Nghiên Huấn - Chapter VP of Academic Affairs: Tr. Maria Nguyễn Vy</li>
+              <li>Thư Ký Đoàn - Chapter Secretary: Tr. Maria Vũ Cynthia</li>
+              <li>Thủ Quỷ Đoàn - Chapter Treasurer: Tr. Giuse Lựu Nguyễn MinhHiếu</li>
+              <li>Ngành Trưởng Ấu Nhi - Division Leader Seedling Specialist: Tr. Maria Nguyễn Bảo Quỳnh</li>
+              <li>Ngành Trưởng Thiếu Nhi - Division Leader Search Specialist: Tr. Phaolô Lê Bảo Tịnh Trần HuyThắng</li>
+              <li>Ngành Trưởng Nghĩa Sĩ - Division Leader Companion Specialist: Tr. Giuse Nguyễn Huy</li>
+              <li>Ngành Trưởng Hiệp Sĩ - Division Leader Knights of the Eucharist Specialist: Tr. Đôminicô Trạch Trần HuyThành</li>
+              <li>Trưởng Ban Trợ Tá - Lay Chaplain-Assistant Head: TT. Lawrence Nguyễn Cường</li>
+            </ul>
+          </div>
+
+          <div className={styles.pastBoardMembers}>
+            <p>2019-2021 Chapter Executive Committee Members</p>
+            <ul>
+              <li>Cha Tuyên Uý - Chapter Chaplain: Rev. Philip Phan Xuân Lành</li>
+              <li>Đoàn Trưởng - Chapter President: Tr. Maria Nguyễn Vy</li>
+              <li>Đoàn Phó Quản Trị - Chapter VP of Administrative Affairs: Tr. Joan of Arc Trần Valentina</li>
+              <li>Đoàn Phó Nghiên Huấn - Chapter VP of Academic Affairs: Tr. Maria Huỳnh Nhung</li>
+              <li>Thư Ký Đoàn - Chapter Secretary: Tr. Đôminicô Trạch Trần HuyThành</li>
+              <li>Thủ Quỷ Đoàn - Chapter Treasurer: Tr. Giuse Lựu Nguyễn MinhHiếu</li>
+              <li>Ngành Trưởng Ấu Nhi - Division Leader Seedling Specialist: Tr. Maria Nguyễn Bảo Quỳnh</li>
+              <li>Ngành Trưởng Thiếu Nhi - Division Leader Search Specialist: Tr. Louis Nguyễn ThiênQuốc</li>
+              <li>Ngành Trưởng Nghĩa Sĩ - Division Leader Companion Specialist: Tr. Giuse Nguyễn Huy</li>
+              <li>Ngành Trưởng Hiệp Sĩ - Division Leader Knights of the Eucharist Specialist: Tr. Đaminh Nguyễn Tài</li>
+              <li>Trưởng Ban Trợ Tá - Lay Chaplain-Assistant Head: TT. Lawrence Nguyễn Cường</li>
+            </ul>
+          </div>
+
+          <div className={styles.pastBoardMembers}>
+            <p>2016-2019 Chapter Executive Committee Members</p>
+            <ul>
+              <li>Cha Tuyên Uý - Chapter Chaplain: Rev. Augstinô Nguyễn Minh Tân</li>
+              <li>Đoàn Trưởng - Chapter President: Tr. Đôminicô Trạch Trần HuyThành</li>
+              <li>Đoàn Phó Quản Trị - Chapter VP of Administrative Affairs: Tr. Giuse Lê Trung</li>
+              <li>Đoàn Phó Nghiên Huấn - Chapter VP of Academic Affairs: Tr. Maria Huỳnh Nhung</li>
+              <li>Thư Ký Đoàn - Chapter Secretary: Tr. Têrêsa Nguyễn Châu</li>
+              <li>Thủ Quỷ Đoàn - Chapter Treasurer: Tr. Giuse Nguyễn Lực</li>
+              <li>Ngành Trưởng Ấu Nhi - Division Leader Seedling Specialist: Tr. Maria Lê Minh Thư</li>
+              <li>Ngành Trưởng Thiếu Nhi - Division Leader Search Specialist: Tr. Louis Nguyễn ThiênQuốc</li>
+              <li>Ngành Trưởng Nghĩa Sĩ - Division Leader Companion Specialist: Tr. Maria Trần Hoa</li>
+              <li>Ngành Trưởng Hiệp Sĩ - Division Leader Knights of the Eucharist Specialist: Tr. Đôminicô Trạch Trần HuyThành</li>
+              <li>Trưởng Ban Trợ Tá - Lay Chaplain-Assistant Head: TT. Lawrence Nguyễn Cường</li>
+            </ul>
+          </div>
+
+
+
       </div>
     </div>
   );
--- src/pages/Resources.tsx
+++ src/pages/Resources.tsx
@@ -2,11 +2,14 @@ import type { JSX } from "react";
 import styles from "./Resources.module.css";
 import regionalPhoto from "../images/regionalphoto.webp";
 import nationalPhoto from "../images/veym.jpg";
+import regionalLogo from "../images/regionallogo.png"
+import nationalLogo from "../images/nationallogo.jpg"
 
 const Resources = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <div className={styles.resource}>
+        <img src={regionalLogo} alt="Regional Logo" className={`${styles.regionalnationallogo} ${styles.regionallogo} `} />
         <h2>Regional Website</h2>
         <p>Liên Đoàn Phaolô Hạnh's Official Webpage</p>
         <a
@@ -23,6 +26,7 @@ const Resources = (): JSX.Element => {
       </div>
 
       <div className={styles.resource}>
+        <img src={nationalLogo} alt="Regional Logo" className={styles.regionalnationallogo} />
         <h2>National Website</h2>
         <p>VIETNAMESE EUCHARISTIC YOUTH MOVEMENT</p>
         <a
